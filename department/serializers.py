d96d31d9ba3156bc6f29c20f94a4eed51efc91ad
azimjnovislom77 <azimjonovislomjon77@gmail.com>
2025-11-11 09:08:33 +0500

    done

--- department/serializers.py
+++ department/serializers.py
@@ -6,14 +6,12 @@ from department.models import Department, DepartmentTypes
 class DepartmentTypesSerializer(serializers.ModelSerializer):
     class Meta:
         model = DepartmentTypes
-        fields = ['id', 'title', 'title_uz', 'title_ru']
+        fields = ['id', 'department', 'title', 'title_uz', 'title_ru']
 
 
 class DepartmentSerializer(serializers.ModelSerializer):
     department_types = DepartmentTypesSerializer(read_only=True, many=True)
-    department_types_id = serializers.PrimaryKeyRelatedField(queryset=DepartmentTypes.objects.all(),
-                                                             source='department_types', many=True, write_only=True)
 
     class Meta:
         model = Department
-        fields = ['id', 'title', 'title_uz', 'title_ru', 'department_types', 'department_types_id']
+        fields = ['id', 'title', 'title_uz', 'title_ru', 'department_types']
