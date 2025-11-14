e6cbb289b34e2c11874fefe37d36d2a32fe7af76
azimjnovislom77 <azimjonovislomjon77@gmail.com>
2025-11-14 15:30:08 +0500

    done

--- reception/views.py
+++ reception/views.py
@@ -1,12 +1,15 @@
 from rest_framework.decorators import action
 from drf_spectacular.utils import extend_schema
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from django.utils import timezone
+from rest_framework.response import Response
+
 from reception.models import Patient
 from reception.serializers import PatientSerializer
 from user.views import PartialPutMixin
+from datetime import timedelta
 
 
 @extend_schema(tags=['Patient'])
@@ -19,11 +22,23 @@ class PatientViewSet(viewsets.ModelViewSet, PartialPutMixin):
     @action(detail=False, methods=['get'])
     def stats(self, request):
         today = timezone.now().date()
+        one_year_ago = today - timedelta(days=365)
+
         qabul_qilinganlar = Patient.objects.filter().count()
         bugungi_bemorlar = Patient.objects.filter(created_at__date=today).count()
         erkaklar = Patient.objects.filter(gender=Patient.GenderChoice.MALE).count()
         ayollar = Patient.objects.filter(gender=Patient.GenderChoice.FEMALE).count()
-        yangi_tugilganlar = Patient.objects.filter(birth_date=today).count()
+        yangi_tugilganlar = Patient.objects.filter(birth_date__gte=one_year_ago).count()
+
+        data = {
+            "qabul_qilinganlar": qabul_qilinganlar,
+            "bugungi_bemorlar": bugungi_bemorlar,
+            "erkaklar": erkaklar,
+            "ayollar": ayollar,
+            "yangi_tugilganlar": yangi_tugilganlar,
+        }
+
+        return Response(data, status=status.HTTP_200_OK)
 
 
 @extend_schema(tags=['Patient'])
