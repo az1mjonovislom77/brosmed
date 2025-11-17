48dff23822698c4d2e4b9843e72fbd8bade180de
azimjnovislom77 <azimjonovislomjon77@gmail.com>
2025-11-17 14:15:48 +0500

    done

--- laboratory/admin.py
+++ laboratory/admin.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
 
-from laboratory.models import Analysis, AnalysisFile
+from laboratory.models import Analysis, AnalysisFile, Result
+
+
+class ResultInline(admin.TabularInline):
+    model = Result
+    extra = 1
 
 
 @admin.register(Analysis)
 class AnalysisAdmin(admin.ModelAdmin):
     list_display = ('id', 'patient', 'status')
+    inlines = [ResultInline]
+
 
 @admin.register(AnalysisFile)
 class AnalysisFileAdmin(admin.ModelAdmin):
     list_display = ('id', 'analysis')
+
+
+@admin.register(Result)
+class ResultAdmin(admin.ModelAdmin):
+    list_display = ('id', 'analysis', 'title', 'norma',)
