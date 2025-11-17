7a78cbd761947a7cc5a2693b93260ab251f80b27
azimjnovislom77 <azimjonovislomjon77@gmail.com>
2025-11-17 14:12:15 +0500

    done

--- laboratory/urls.py
+++ laboratory/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from laboratory.views import AnalysisViewSet, ResultViewSet
+from laboratory.views import AnalysisViewSet, ResultViewSet, ResultByDepartmentDetailAPIView
 
 router = DefaultRouter()
 router.register('analysis', AnalysisViewSet)
@@ -9,4 +9,7 @@ router.register('result', ResultViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('department/result/<int:department_type_id>/', ResultByDepartmentDetailAPIView.as_view(),
+         name='results-by-department'),
+
 ]
--- laboratory/views.py
+++ laboratory/views.py
@@ -9,6 +9,7 @@ from laboratory.serializers import AnalysisSerializer, AnalysisPostSerializer, A
 from user.views import PartialPutMixin
 from rest_framework.response import Response
 from django.db.models import Q
+from rest_framework import generics
 
 
 @extend_schema(tags=['Analysis'])
@@ -78,3 +79,12 @@ class ResultViewSet(viewsets.ModelViewSet, PartialPutMixin):
     serializer_class = ResultSerializer
     permission_classes = [IsAuthenticated]
     http_method_names = ['get', 'post', 'put', 'delete']
+
+
+@extend_schema(tags=['Result'])
+class ResultByDepartmentDetailAPIView(generics.ListAPIView):
+    serializer_class = ResultSerializer
+
+    def get_queryset(self):
+        department_type_id = self.kwargs.get('department_type_id')
+        return Result.objects.filter(analysis__department_types_id=department_type_id)
