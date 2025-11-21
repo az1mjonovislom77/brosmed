7279471682eec03d025fcd3da97984555e0ba3ca
azimjnovislom77 <azimjonovislomjon77@gmail.com>
2025-11-21 09:51:03 +0500

    done

--- doctor/models.py
+++ doctor/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-
-from reception.models import Patient
 from user.models import User
+from reception.models import Patient
 
 
 class Consultations(models.Model):
--- doctor/translation.py
+++
@@ -1,8 +0,0 @@
-from modeltranslation.translator import register, TranslationOptions
-
-from doctor.models import Consultations
-
-
-@register(Consultations)
-class ConsultationsTranslation(TranslationOptions):
-    fields = ('diagnosis', 'recommendation', 'recipe')
