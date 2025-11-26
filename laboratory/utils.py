f8ae6329bfa1d45f4bc0883415f53617d80a8c16
azimjnovislom77 <azimjonovislomjon77@gmail.com>
2025-11-26 16:10:16 +0500

    done

--- laboratory/utils.py
+++ laboratory/utils.py
@@ -38,8 +38,7 @@ def create_analysis_docx(patient, analysis, results_list, output_path, header_im
         # O'ng ustunga matn
         cell_text = table.cell(0, 1)
         p_text = cell_text.paragraphs[0]
-        text = ("MANZIL: QARSHI SHAHAR KAT - MFY, NASAF KO' CHASI, 31-UY\n"
-                "TEL: (75) 223-47-47\n"
+        text = ("MANZIL: QARSHI SHAHAR KAT - MFY, NASAF KO' CHASI, 31-UY TEL: (75) 223-47-47\n"
                 "MoBIL: (97) 070-47-47 ; (97) 310-21-01")
         run_text = p_text.add_run(text)
         run_text.font.size = Pt(10)
