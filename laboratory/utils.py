0d662ac7bb2260bb0e00dd941b7f945df10a7378
azimjnovislom77 <azimjonovislomjon77@gmail.com>
2025-11-26 16:25:04 +0500

    done

--- laboratory/utils.py
+++ laboratory/utils.py
@@ -41,7 +41,7 @@ def create_analysis_docx(patient, analysis, results_list, output_path, header_im
         text = ("MANZIL: QARSHI SHAHAR KAT - MFY, NASAF KO' CHASI, 31-UY TEL: (75) 223-47-47\n"
                 "MoBIL: (97) 070-47-47 ; (97) 310-21-01")
         run_text = p_text.add_run(text)
-        run_text.font.size = Pt(11)
+        run_text.font.size = Pt(10)
         p_text.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
 
         doc.add_paragraph()  # biroz space qo'shamiz
