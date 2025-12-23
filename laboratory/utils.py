dc80c1cd817797b8db9a2089571b6c4109e8325e
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2025-12-23 11:56:59 +0500

    done

--- laboratory/utils.py
+++ laboratory/utils.py
@@ -146,7 +146,7 @@ def create_analysis_docx(patient, analysis, results_list, output_path, header_im
     left_cell = sign_table.cell(0, 0)
     left_p = left_cell.paragraphs[0]
     left_p.add_run("Врач лаборант:  __________________")
-    left_p.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
+    left_p.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
 
     right_cell = sign_table.cell(0, 1)
     right_p = right_cell.paragraphs[0]
