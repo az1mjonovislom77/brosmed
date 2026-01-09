e20bf8ff7f2bf6d7bbd4dd8edd00ea9033560c1b
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-01-09 15:22:15 +0500

    done

--- laboratory/views.py
+++ laboratory/views.py
@@ -4,7 +4,6 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
-t
 from laboratory.serializers import AnalysisSerializer, AnalysisPostSerializer, AnalysisSearchInputSerializer, \
     AnalysisFullDetailSerializer, AnalysisDetailInputSerializer
 from reception.models import Analysis
@@ -65,6 +64,7 @@ class AnalysisViewSet(viewsets.ModelViewSet, PartialPutMixin):
 
         output = AnalysisSerializer(queryset, many=True, context={'request': request})
         return Response(output.data)
+
 
 @csrf_exempt
 def check_patient(request):
