27410bdfc38b3344bad5a393cf6455d6b2122904
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-02-09 13:00:55 +0500

    done

--- laboratory/views.py
+++ laboratory/views.py
@@ -18,7 +18,7 @@ from user.views.user_views import PartialPutMixin
 
 
 class AnalysisPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 20
     page_size_query_param = 'page_size'
 
 
