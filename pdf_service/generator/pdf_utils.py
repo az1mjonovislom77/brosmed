6c73de06685c1a94f2f5787c165d2d15b77a7978
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-05-07 15:11:09 +0500

    done

--- pdf_service/generator/pdf_utils.py
+++ pdf_service/generator/pdf_utils.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from django.conf import settings
 from docx import Document
 from docx.shared import Pt, Inches
 from docx.enum.table import WD_TABLE_ALIGNMENT
@@ -19,18 +20,20 @@ def convert_docx_to_pdf(docx_path: str):
 
 def create_analysis_docx(patient, analysis, results_list, output_path, header_image_path=None, analysis_title="Analiz"):
     doc = Document()
+
     section = doc.sections[0]
     section.top_margin = Inches(0.4)
     section.bottom_margin = Inches(0.4)
     section.left_margin = Inches(0.5)
     section.right_margin = Inches(0.5)
+
     style = doc.styles["Normal"]
     style.font.name = "Times New Roman"
     style._element.rPr.rFonts.set(qn("w:eastAsia"), "Times New Roman")
     style.font.size = Pt(11)
 
     if header_image_path is None:
-        header_image_path = "/app/laboratory/logo.jpg"
+        header_image_path = os.path.join(settings.BASE_DIR, "laboratory", "logo.jpg")
 
     if os.path.exists(header_image_path):
         table = doc.add_table(rows=1, cols=2)
@@ -57,6 +60,7 @@ def create_analysis_docx(patient, analysis, results_list, output_path, header_im
 
     info_table = doc.add_table(rows=4, cols=2)
     info_table.style = "Table Grid"
+
     info_table.cell(0, 0).text = "Bemor I.F.O"
     info_table.cell(0, 1).text = (
         f"{patient.get('name', '')} "
@@ -85,6 +89,7 @@ def create_analysis_docx(patient, analysis, results_list, output_path, header_im
     seen_titles = set()
 
     for item in results_list:
+
         title = item.get("title", "").strip()
 
         if not title:
@@ -94,6 +99,7 @@ def create_analysis_docx(patient, analysis, results_list, output_path, header_im
             continue
 
         seen_titles.add(title)
+
         value = str(item.get("value", "")).strip()
 
         if not value:
@@ -120,12 +126,15 @@ def create_analysis_docx(patient, analysis, results_list, output_path, header_im
 
         p = doc.add_paragraph("Natija hali kiritilmagan")
         p.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+
         run = p.runs[0]
         run.italic = True
         run.font.size = Pt(12)
 
     else:
+
         table = doc.add_table(rows=1, cols=3, style="Table Grid")
+
         hdr = table.rows[0].cells
         hdr[0].text = "Название анализа"
         hdr[1].text = "Результат анализа"
@@ -133,13 +142,16 @@ def create_analysis_docx(patient, analysis, results_list, output_path, header_im
 
         for item in valid_results:
             row = table.add_row().cells
+
             row[0].text = item["title"]
             row[1].text = item["value"]
             row[2].text = item["norma"]
 
     sign_table = doc.add_table(rows=1, cols=2)
+
     left_cell = sign_table.cell(0, 0)
     left_cell.paragraphs[0].add_run("Врач лаборант: _____________________")
+
     right_cell = sign_table.cell(0, 1)
     right_cell.paragraphs[0].add_run(analysis.get("doctor", ""))
 
